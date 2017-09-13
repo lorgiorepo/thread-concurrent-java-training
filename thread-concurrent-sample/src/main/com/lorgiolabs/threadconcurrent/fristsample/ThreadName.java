4f35ab4e65d71e5d0f1049225ff4c4b123d2fae0
Lorgio Trinidad <lorgiotrinidad@gmail.com>
2017-09-13 19:20:24 -0300

    Add thread properties

--- thread-concurrent-sample/src/main/com/lorgiolabs/threadconcurrent/fristsample/ThreadName.java
+++ thread-concurrent-sample/src/main/com/lorgiolabs/threadconcurrent/fristsample/ThreadName.java
@@ -8,7 +8,9 @@ public class ThreadName {
     public static void main(String[] args) {
         Thread mainTread = Thread.currentThread();
         mainTread.setName("Lorgio Thread");
+        System.out.println(mainTread.getId());
         System.out.println(mainTread.getName());
+        System.out.println(mainTread.getPriority());
+        System.out.println(mainTread.getState());
     }
 }
- 
